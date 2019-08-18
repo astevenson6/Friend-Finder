332f0dc2a03421860fa76589b6811355e8967fae
astevenson6 <astevenson6@yahoo.com>
2019-08-18 13:53:24 -0700

    html routes finished start doing data/api

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,27 +1,11 @@
-function handleRequest(req, res) {
+var path = require("path");
 
-    // Capture the url the request is made to
-    var path = req.url;
-  
-    // When we visit different urls, read and respond with different files
-    switch (path) {
-  
-    case "/survey":
-      return fs.readFile(__dirname + "/survey.html", function(err, data) {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      });
-
-      // default to rendering index.html, if none of above cases are hit
-    default:
-      return fs.readFile(__dirname + "/home.html", function(err, data) {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      });
-    }
-  }
-
-  server.listen(PORT, function() {
-    console.log("Server is listening on PORT: " + PORT);
+module.exports = function (app) {
+  app.get("/survey", function(req,res) {
+    res.sendFile(path.join(__dirname, "/../public/survey.html"));
   });
-  
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+  });
+};
